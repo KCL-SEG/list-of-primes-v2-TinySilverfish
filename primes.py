f428849f211c9ff3dc0ab0a305405e0ac68afe62
TinySilverfish <tinysilverfish@gmail.com>
2023-10-05 20:21:06 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,18 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
-    list = []
-    return list
+	list = []
+
+	i = 2
+	while len(list) < number_of_primes:
+		for j in range(2, i):
+			if i % j == 0:
+				break
+		else:
+			list.append(i)
+		i += 1
+	return list
+
+print(primes(10))
