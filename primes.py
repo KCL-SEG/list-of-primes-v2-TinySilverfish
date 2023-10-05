aaaed20cfc8869de5621ad8a2520b58a428a5d3d
TinySilverfish <tinysilverfish@gmail.com>
2023-10-05 20:22:54 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -5,6 +5,9 @@
 def primes(number_of_primes):
 	list = []
 
+	if number_of_primes < 1:
+		raise ValueError("Number of primes must be greater than 0")
+	
 	i = 2
 	while len(list) < number_of_primes:
 		for j in range(2, i):
@@ -15,4 +18,4 @@ def primes(number_of_primes):
 		i += 1
 	return list
 
-print(primes(10))
+print(primes(0))
